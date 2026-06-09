9b5fdc4e04b90dc595846fc8b851ddf0cae2dea6
Joao Gabriel <gabriel.122.gm85@gmail.com>
2026-06-09 13:05:13 -0400

    feat: adiciona validações no DTO de criação de usuário

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,21 @@
 import { PapelUsuario } from '@prisma/client';
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'O campo nome deve ser preenchido' })
+  @IsString()
   nome!: string;
+
+  @IsNotEmpty({ message: 'O campo email deve ser preenchido' })
+  @IsEmail({}, { message: 'O campo email deve ser um endereço de email válido' })
   email!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'O campo senha deve ser preenchido' })
+  @MinLength(6)
   senha!: string;
+
+  @IsOptional()
+  @IsEnum(PapelUsuario)
   papel?: PapelUsuario;
 }
