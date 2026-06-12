509fe9284bd72f977fe9fed8ec88eebda776a08e
Joao Gabriel <gabriel.122.gm85@gmail.com>
2026-06-12 09:24:11 -0400

    feat: adiciona crud completo de freelancer

--- src/freelancers/dto/create-freelancer.dto.ts
+++ src/freelancers/dto/create-freelancer.dto.ts
@@ -1,1 +1,20 @@
-export class CreateFreelancerDto {}
+import { IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateFreelancerDto {
+
+  @IsOptional()
+  @IsString({ message: 'O campo titulo deve ser uma texto' })
+  titulo?: string;
+
+  @IsOptional()
+  @IsString({ message: 'O campo biografia deve ser uma texto' })
+  biografia?: string;
+
+  @IsOptional()
+  @IsNumber()
+  valorHora?: number;
+
+  @IsOptional()
+  @IsString({ message: 'O campo telefone deve ser uma texto' })
+  telefone?: string;
+}
--- src/freelancers/freelancers.controller.ts
+++ src/freelancers/freelancers.controller.ts
@@ -1,34 +1,52 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { FreelancersService } from './freelancers.service';
 import { CreateFreelancerDto } from './dto/create-freelancer.dto';
 import { UpdateFreelancerDto } from './dto/update-freelancer.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { PapelUsuario } from '@prisma/client';
+import { Roles } from '../auth/decorators/roles.decorator';
 
+type RequestComUsuario = Request & {
+  user: {
+    id: string;
+    email: string;
+    papel: PapelUsuario;
+  }
+}
+
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('freelancers')
 export class FreelancersController {
-  constructor(private readonly freelancersService: FreelancersService) {}
+  constructor(private readonly freelancersService: FreelancersService) { }
 
+  @Roles(PapelUsuario.FREELANCER)
   @Post()
-  create(@Body() createFreelancerDto: CreateFreelancerDto) {
-    return this.freelancersService.create(createFreelancerDto);
+  create(@Body() createFreelancerDto: CreateFreelancerDto, @Req() req: RequestComUsuario) {
+    return this.freelancersService.create(req.user.id, createFreelancerDto);
   }
 
+  @Roles(PapelUsuario.ADMIN)
   @Get()
   findAll() {
     return this.freelancersService.findAll();
   }
 
+  @Roles(PapelUsuario.FREELANCER, PapelUsuario.ADMIN)
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.freelancersService.findOne(+id);
+    return this.freelancersService.findOne(id);
   }
 
+  @Roles(PapelUsuario.FREELANCER)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateFreelancerDto: UpdateFreelancerDto) {
-    return this.freelancersService.update(+id, updateFreelancerDto);
+    return this.freelancersService.update(id, updateFreelancerDto);
   }
 
+  @Roles(PapelUsuario.FREELANCER, PapelUsuario.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.freelancersService.remove(+id);
+    return this.freelancersService.remove(id);
   }
 }
